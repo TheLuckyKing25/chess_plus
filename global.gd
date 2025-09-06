8dcd82d330b483b30aa442acaf3ebfa16a8efe98
TheLuckyKing25 <theluckyking20@gmail.com>
2025-09-06 13:19:16 -0800

    Added comments to important scripts

--- global.gd
+++ global.gd
@@ -1,6 +1,9 @@
+# Script contains global variables which can be accessed and changed from any other script.
 extends Node
 
 var is_piece_selected = true
+
+# sets the variables to the objects selected, allowing them to be addressed in other scripts.
 var piece_selected = null
 var tile_selected = null
 	
--- tile.gd
+++ tile.gd
@@ -2,6 +2,7 @@ extends AnimatableBody3D
 
 @export var camera: Camera3D
 
+# checks if a piece has been selected and the tile has been clicked on.
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if Global.piece_selected != null and (event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT):
 		var mouse_pos = event.position
@@ -13,6 +14,10 @@ func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, n
 			var clicked_object = result.collider
 			Global.tile_selected = clicked_object
 			print(clicked_object)
+			
+			# Moves the selected piece the the selected tile by matching the piece's XZ coordinates with 
+			# the tile's XZ coordinates.  
+			# this should be moved to the Pawn script since it affects the Pawn.
 			if Global.piece_selected != null:
 				Global.piece_selected.global_position = Global.tile_selected.global_position * Vector3(1,0,1)
 				Global.tile_selected = null
