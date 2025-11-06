b080545e96e68c61ad1b991c7c44ea52320c90b4
Kyrneth <skylercarman124@gmail.com>
2025-11-06 14:36:13 -0900

    Add start game menu with singleplayer, host, and join options
    Introduces a new 'Start' menu to the main menu scene, including buttons for Singleplayer, Host Game, and Join Game, as well as an input for host IP. Updates script logic to show/hide the new menu and handle singleplayer selection.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,10 +2,12 @@ extends Control
 
 func _input(event) -> void:
 	if event.is_action_pressed("ui_cancel") and $Main.visible == false:
+		$Start.hide()
 		$Main.show()
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/gameEnvironment.tscn")
+	$Main.hide()
+	$Start.show()
 
 func _on_settings_pressed():
 	$Options/Settings.show()
@@ -13,3 +15,6 @@ func _on_settings_pressed():
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+func _on_singleplayer_pressed():
+	get_tree().change_scene_to_file("res://scenes/gameEnvironment.tscn")
